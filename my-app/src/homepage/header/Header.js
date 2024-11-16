753b9e432ae22b40292b082fe1cf18a5595dc48a
roksolanamaksymovych <151615831+roksolanamaksymovych@users.noreply.github.com>
2024-11-16 20:06:33 +0200

    Create Header.js

---
+++ my-app/src/homepage/header/Header.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './Header.css';
+import Nav from '../navigation/Nav';
+
+
+
+const Header = () => {
+    return (
+        <header className="header">
+            <div className="logo">Paradise</div>
+            <Nav />
+        </header>
+    );
+};
+
+export default Header;
