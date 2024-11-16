73981b1c7dfd636dc2aaf06afa230ee920991aa3
roksolanamaksymovych <151615831+roksolanamaksymovych@users.noreply.github.com>
2024-11-16 19:40:28 +0200

    Create Nav.js

---
+++ my-app/src/homepage/navigation/Nav.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './Nav.css'
+
+const Nav = () => {
+    return (
+        <nav className="nav">
+            <a href="#">Home</a>
+            <a href="#">Catalog</a>
+            <a href="#">Cart</a>
+        </nav>
+    );
+};
+
+export default Nav;
